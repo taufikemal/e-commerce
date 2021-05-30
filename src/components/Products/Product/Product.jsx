418b537ade4f648e65c9a277fd4b92781f0c5833
Taufik Kemal Thaha <artcrev7@gmail.com>
2021-05-30 22:58:01 +0800

    Change navbar color

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/icons'
-import useStyles from './styles'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = ({ product }) => {
-	const classes = useStyles();
+import useStyles from './styles';
 
-	return (
-		<Card className={classes.root}>
-			<CardMedia className={classes.media} image={product.image} title={product.name} />
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
 
-			<CardContent>
-				<div className={classes.cardContent}>
-					<Typography variant="h5" gutterBottom>
-						{product.name}
-					</Typography>
+  const handleAddToCart = () => onAddToCart(product.id, 1);
 
-					<Typography variant="h5">
-						{product.price}
-					</Typography>
-				</div>
-
-				<Typography variant="body2" color="textSecondary">
-					{product.description}
-				</Typography>
-			</CardContent>
-
-			<CardActions disableSpacing className={classes.cardActions}>
-				<IconButton aria-label="Add to Cart">
-					<AddShoppingCart />
-				</IconButton>
-			</CardActions>
-		</Card>
-	)
-}
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Rp{product.price.formatted}
+          </Typography>
+        </div>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default Product;
+
